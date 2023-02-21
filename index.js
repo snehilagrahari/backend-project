ec41bb76d6f2ca0af46ad394b17f8edd3f0f22b5
snehilagrahari <sag21062001@outlook.com>
2023-02-22 01:05:54 +0530

    backend Complete

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const {userRouter} = require('./routes/user.routes')
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use('/users',userRouter);
 
